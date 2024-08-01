97aaf38387b575161230cfe79d0b3149841d4f6f
Mrpradhanji <114905691+Mrpradhanji@users.noreply.github.com>
2024-08-02 00:28:50 +0530

    Added customized logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -57,7 +57,7 @@ const Header = () => {
       <div className=' h-full container mx-auto flex items-center px-4 justify-between'>
             <div className=''>
                 <Link to={"/"}>
-                    <Logo w={90} h={50}/>
+                    <Logo w={60} h={50}/>
                 </Link>
             </div>
 
