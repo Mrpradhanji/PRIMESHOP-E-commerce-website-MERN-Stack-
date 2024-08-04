e6d8d60f3ae36f06805f997189924716af1dc373
Mrpradhanji <114905691+Mrpradhanji@users.noreply.github.com>
2024-08-04 22:28:33 +0530

    Testing abd bug solve

--- backend/helpers/permission.js
+++ backend/helpers/permission.js
@@ -2,7 +2,7 @@ const userModel = require("../models/userModel")
 
 const uploadProductPermission = async(userId) => {
     const user = await userModel.findById(userId)
-
+    //Only Admin can add Products Manually
     if(user.role === 'ADMIN'){
         return true
     }
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-slate-200'>
-      <div className='container mx-auto p-4'>
-       <p className='text-center font-bold' title="Youtube Channel">Dynamic Coding with Amit</p>
-      </div>
-    </footer>
+    <div>
+      
+    </div>
   )
 }
 
